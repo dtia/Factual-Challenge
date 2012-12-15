453ada0644b6dedaf7b0a00802f358965236a750
Derek Tia <dtia05@gmail.com>
2012-12-15 15:51:19 -0800

    clean up code

--- Hangman/src/Hangman/PlayHangman.java
+++ Hangman/src/Hangman/PlayHangman.java
@@ -5,22 +5,22 @@ import java.util.HashMap;
 import Hangman.HangmanGame.Status;
 
 public class PlayHangman {
-
+	public static boolean DEBUG = false;
+	
 	/**
 	 * @param args
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
-		String[] words1 = {"comaker", "monadism", "cumulate", "eruptive", "factual", "mus", "nagging", 
-				"oses", "remembered"};
-		String[] words2 = {"spodumenes", "stereoisomers", "toxics", "trichromats", "triose", "uniformed"};
-		String[] words3 = {"remembered"};
-		
-		// 25: toxics, remembered, nagging, mus
-		
-		System.out.println(playWords(words1));
+		String[] words = {"comaker", "monadism", "cumulate", "eruptive", "factual", "mus", "nagging", 
+				"oses", "remembered", "spodumenes", "stereoisomers", "toxics", "trichromats", "triose", "uniformed"};
+				
+		System.out.println(playWords(words));
 	}
 	
+	/*
+	 * Play a new game of hangman for each of the words in the words array
+	 */
 	private static HashMap<String,Integer> playWords(String[] words) throws IOException {
 		HashMap<String,Integer> wordScores = new HashMap<String,Integer>();
 		
@@ -34,17 +34,18 @@ public class PlayHangman {
 		return wordScores;
 	}
 	
-	// runs your strategy for the given game, then returns the score
+	/*
+	 * Runs the strategy for the given game, then returns the score
+	 */
 	public static int run(HangmanGame game, GuessingStrategy strategy) {
 	  while(game.gameStatus() == Status.KEEP_GUESSING) {
 		  System.out.println(game);
-		  Guess nextGuess = strategy.nextGuess(game);  
+		  Guess nextGuess = strategy.nextGuess(game);
+		  if(DEBUG)
+			  System.out.println(nextGuess);
 		  nextGuess.makeGuess(game);
 	  }
 	  System.out.println(game);
 	  return game.currentScore();
 	}
-
-
-
 }
--- Hangman/src/Hangman/WinningStrategy.java
+++ Hangman/src/Hangman/WinningStrategy.java
@@ -7,20 +7,26 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
 public class WinningStrategy implements GuessingStrategy {
+	// ***NOTE: This file path must be modified to point to the words.txt file on your machine***
 	private static String fileName = "/Users/dtia/Development/eclipse workspace/factual challenge/Hangman/src/words.txt";
 	
-	private List<Character> mainVowels = new ArrayList<Character>(Arrays.asList('E', 'A', 'I', 'O',  'U'));
-	private List<Character> consonants = new ArrayList<Character>(Arrays.asList('T', 'N', 'H', 'R', 'S', 'D', 'M', 'L', 'F', 'C', 'G', 'Y', 'P', 'W', 'B', 'V', 'K', 'J', 'X', 'Z', 'Q'));
+	private static char[] VOWELS = {'E', 'A', 'I', 'O',  'U'}; 
+	private List<Character> vowels = new ArrayList<Character>(Arrays.asList('E', 'A', 'I', 'O', 'U'));
+	private List<Character> consonants = new ArrayList<Character>(Arrays.asList('T', 'N', 'H', 'R', 'S', 'M', 'D', 'L', 'C', 'F', 'G', 'Y', 'P', 'W', 'B', 'V', 'K', 'J', 'X', 'Z', 'Q'));
 	
-	private static HashMap<Integer, List<String>> dict = new HashMap<Integer, List<String>>();
+	private HashMap<Integer, List<String>> dict = new HashMap<Integer, List<String>>();
 	private List<String> sameLengths = new ArrayList<String>();
 	
 	public WinningStrategy() throws IOException {
 		loadDictionary();
 	}
 	
+	/*
+	 * Read text file into dictionary
+	 */
 	private void loadDictionary() throws IOException {
 	   BufferedReader br = new BufferedReader(new FileReader(fileName));
 	    try {
@@ -53,31 +59,121 @@ public class WinningStrategy implements GuessingStrategy {
 			sameLengths = dict.get(game.getSecretWordLength());
 		}
 		
-		// remove non matches from list
-		removeNonMatches(game.getGuessedSoFar());
-		System.out.println("same lengths size: " + sameLengths.size());		
-		//System.out.println("same length words: " + sameLengths);
-		// guess word if there are 3 possibilities
+		// filter non matches from list
+		filterNonMatches(game.getGuessedSoFar());
+		
+		if (PlayHangman.DEBUG) {
+			System.out.println("filtered words size: " + sameLengths.size());		
+			//System.out.println("filtered words words: " + sameLengths);
+		}
+				
+		// guess random word if there are 4 or less possibilities
 		if (sameLengths.size() <= 4) {
-			// guess a random word
 			int min = 0;
 			int max = sameLengths.size()-1;
 			int randIndex = min + (int)(Math.random() * ((max - min) + 1));
 			String randomWord = sameLengths.remove(randIndex);			
 			return new GuessWord(randomWord);
 		}
-		
-		if (mainVowels.size() > 0) {
-			// guess another vowel
-			return new GuessLetter(mainVowels.remove(0));
-		}		
-		else {
-			// guess a common letter
-			return new GuessLetter(consonants.remove(0));
+		// start guessing vowels
+		else if (vowels.size() > 0 && initialVowelLogic(game)) {
+			// guess vowel in frequency order
+			return new GuessLetter(vowels.remove(0));
 		}
+		// RE and ER are common word pairs so guessing R when there is an E
+		else if (game.getCorrectlyGuessedLetters().contains('E')) {
+			if (!game.getAllGuessedLetters().contains(new Character('R'))) {
+				consonants.remove(new Character('R'));
+				return new GuessLetter('R');
+			}
+		}
+		// consider whether another vowel should be added
+		else if (vowels.size() > 0 && secondaryVowelLogic(game)) {
+			return new GuessLetter(vowels.remove(0));
+		}
+		
+		// guess a common letter in frequency order
+		return new GuessLetter(consonants.remove(0));
 	}
 	
-	private void removeNonMatches(String currentGuess) {
+	/*
+	 * Count the number of letters remaining to be guessed
+	 */
+	private int numMysteryLetters(String guessedSoFar) {
+		int numMysteryLetters = 0;
+		for(int i=0; i<guessedSoFar.length(); i++) {
+			if (guessedSoFar.charAt(i) == HangmanGame.MYSTERY_LETTER)
+				numMysteryLetters++;
+		}
+		return numMysteryLetters;
+	}
+	
+	/*
+	 * Guess a vowel if:
+	 * 		1) There are less than 2 vowels guessed already AND
+	 * 		2) The number of vowels guessed is less than 1/3 the length of the word
+	 */
+	private boolean initialVowelLogic(HangmanGame game) {
+		return uniqueVowelCount(game.getCorrectlyGuessedLetters()) < 2 &&
+				(float)wordVowelCount(game.getGuessedSoFar()) / game.getSecretWordLength() < .3;				
+	}
+	
+	/*
+	 * Guess a vowel if:
+	 * 		1) The number of guesses left are less than 1/4 remaining guesses AND
+	 * 		2) The number of missing letters is more than half the length of the word
+	 */
+	private boolean secondaryVowelLogic(HangmanGame game) {
+		return game.numWrongGuessesRemaining() < 0.25 * game.getMaxWrongGuesses() &&
+				numMysteryLetters(game.getGuessedSoFar()) / game.getSecretWordLength() > 0.5;
+	}
+	
+	/*
+	 * Count how many vowels are in the currently guessed word
+	 */
+	private int wordVowelCount(String guessedSoFar) {
+		int vowelCount = 0;
+		for (int i=0; i<guessedSoFar.length(); i++) {
+			if (isVowel(guessedSoFar.charAt(i))) 
+				vowelCount++;
+		}
+		return vowelCount;
+	}
+	
+	/*
+	 * Determine if this letter is a vowel
+	 */
+	private boolean isVowel(char letter) {
+		boolean isVowel = false;
+		for(int i=0; i<VOWELS.length; i++) {
+			if(VOWELS[i] == letter)
+				isVowel = true;
+		}
+		return isVowel;
+	}
+	
+	/*
+	 * Determine how many unique vowels are in the set of letters
+	 */
+	private int uniqueVowelCount(Set<Character> letters) {
+		int vowelCount = 0;
+		if(letters.contains('A'))
+			vowelCount++;
+		if(letters.contains('E'))
+			vowelCount++;
+		if(letters.contains('I'))
+			vowelCount++;
+		if(letters.contains('O'))
+			vowelCount++;
+		if(letters.contains('U'))
+			vowelCount++;
+		return vowelCount;
+	}
+	
+	/*
+	 * Filter words that don't have the same letters in the same positions
+	 */
+	private void filterNonMatches(String currentGuess) {
 		List<String> potentialMatches = new ArrayList<String>();
 		for(int i=0; i<sameLengths.size(); i++) {
 			if(isPotentialMatch(currentGuess, sameLengths.get(i)))
@@ -86,11 +182,13 @@ public class WinningStrategy implements GuessingStrategy {
 		sameLengths = potentialMatches;
 	}
 	
+	/*
+	 * Determine if the guessed word has the same letters in the same positions
+	 */
 	private boolean isPotentialMatch(String currentGuess, String dictWord) {
 		for(int i=0; i<dictWord.length(); i++) {			
 			if(currentGuess.charAt(i) != HangmanGame.MYSTERY_LETTER &&
 				currentGuess.charAt(i) != dictWord.toUpperCase().charAt(i)) { 
-				//System.out.println(currentGuess + " " + dictWord + " " + currentGuess.charAt(i) + " " + dictWord.toUpperCase().charAt(i));
 				return false;			
 			}
 		}		
